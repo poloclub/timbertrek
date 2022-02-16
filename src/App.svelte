f6bdd065f5a3ec5ea850abf52b78432ef98ddb67
Jay Wang <jay@zijie.wang>
2022-02-16 17:24:57 -0500

    Init commit
    Signed-off-by: Jay Wang <jay@zijie.wang>

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+
+</svelte:head>
+
+<script lang="ts">
+  import Finder from './components/finder/Finder.svelte';
+</script>
+
+<style lang='scss'>
+  .stand-alone-page {
+    color: #333;
+    box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 1.5;
+  }
+</style>
+
+<div class='stand-alone-page'>
+	<Finder />
+</div>
---
+++ src/components/finder/Finder.svelte
@@ -0,0 +1,40 @@
+<script lang='ts'>
+  import {onMount} from 'svelte';
+
+  onMount(() => {
+    // console.log('mounted!');
+  });
+
+</script>
+
+<style lang='scss'>
+  @import './Finder.scss';
+</style>
+
+<div class='main-app'>
+
+  <div class='finder-wrapper'>
+
+    <div class='header'>
+
+      <div class='logo'>
+        <div class='logo-icon'>
+          Tree Finder
+        </div>
+      </div>
+    </div>
+
+    <div class='content'>
+
+      <div class='ring'>
+
+      </div>
+
+      <div class='feature-list'>
+
+      </div>
+
+    </div>
+  </div>
+
+</div>
