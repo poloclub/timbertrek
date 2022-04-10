555e240e23b808aee6a3d8d84afe608f7a1471f8
Jay Wang <jay@zijie.wang>
2022-04-10 17:00:53 -0400

    Bump version: 0.1.2 → 0.1.3

--- notebook-widget/setup.py
+++ notebook-widget/setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/xiaohk/timbertrek",
-    version="0.1.2",
+    version="0.1.3",
     zip_safe=False,
 )
