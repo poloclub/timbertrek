0f64c11188df0906b4c58ac3848db1fe278f0127
Jay Wang <jay@zijie.wang>
2022-08-24 16:59:37 -0400

    Deploy d6f2b5f8539659e7314d82b679d7dbd0a37a4069

---
+++ notebook/build/9866.9a6cafe.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_JUPYTERLAB_CORE_OUTPUT=self.webpackChunk_JUPYTERLAB_CORE_OUTPUT||[]).push([[9866],{89866:(t,a,e)=>{e.r(a),e.d(a,{default:()=>T});var o=e(68161),r=e(97470),s=e(82002);const T={id:"@jupyterlab/celltags",autoStart:!0,requires:[o.INotebookTools,o.INotebookTracker,s.ITranslator],activate:(t,a,e,o)=>{const s=new r.TagTool(e,t,o);a.addItem({tool:s,rank:1.6})}}}}]);
+//# sourceMappingURL=9866.9a6cafe.js.map
